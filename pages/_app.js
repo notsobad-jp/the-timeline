76e506bf95d3fb6ff34fbfba97835a4e82fa234a
notsobad <tomomichi.onishi@gmail.com>
2020-07-11 14:44:52 +0900

    change header on timelines/show

--- pages/_app.js
+++ pages/_app.js
@@ -5,15 +5,14 @@ import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from '../src/theme';
 import { auth, firestore, firebase } from '../lib/firebase.js'
-// import { useAuthState } from "react-firebase-hooks/auth";
 import Header from "../components/header.js"
 import "vis-timeline/styles/vis-timeline-graph2d.css";
 import "../styles/timeline.css";
+import { useRouter } from 'next/router'
 
 
 
 export default function MyApp(props) {
-  // const [user, initialising, error] = useAuthState(firebase.auth());
   const { Component, pageProps } = props;
 
   React.useEffect(() => {
@@ -24,17 +23,19 @@ export default function MyApp(props) {
     }
   }, []);
 
+  const router = useRouter()
+
   return (
     <React.Fragment>
       <Head>
-        <title>My page</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
       <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
-          <Header />
-          <Component {...pageProps} />
+        { router.pathname != '/timelines/[id]' &&
+            <Header />
+        }
+        <Component {...pageProps} />
       </ThemeProvider>
     </React.Fragment>
   );
