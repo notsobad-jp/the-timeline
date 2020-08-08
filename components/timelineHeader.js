04c4362462c3b37305b946460c2d7a153d4eeeeb
notsobad <tomomichi.onishi@gmail.com>
2020-08-08 19:34:04 +0900

    wip: add share dialog

--- components/timelineHeader.js
+++ components/timelineHeader.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { useRouter } from 'next/router'
 import { makeStyles } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useTheme } from '@material-ui/core/styles';
 import { auth, firestore, firebase } from '../lib/firebase.js'
 import Link from '../src/Link';
 import AppBar from '@material-ui/core/AppBar';
@@ -12,6 +14,13 @@ import Tooltip from '@material-ui/core/Tooltip';
 import FormatAlignLeftIcon from '@material-ui/icons/FormatAlignLeft';
 import GridOnIcon from '@material-ui/icons/GridOn';
 import ShareIcon from '@material-ui/icons/Share';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import FacebookIcon from '@material-ui/icons/Facebook';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -24,11 +33,34 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
   },
+  snsButton: {
+    color: "#fff",
+    paddingRight: theme.spacing(2),
+    paddingLeft: theme.spacing(2),
+    "&:hover": {
+      opacity: 0.9,
+    },
+    "&:first-child": {
+      marginRight: theme.spacing(2),
+    },
+    "& svg": {
+      marginRight: theme.spacing(1),
+    },
+  },
+  twitter: {
+    backgroundColor: "#2196f3 !important",
+  },
+  facebook: {
+    backgroundColor: "#3B5998 !important",
+  },
 }));
 
 export default function TimelineHeader({title, sourceUrl}){
   const classes = useStyles();
   const router = useRouter();
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
+  const [dialogOpened, setDialogOpened] = useState(false);
 
   return (
     <div className={classes.root}>
@@ -47,12 +79,39 @@ export default function TimelineHeader({title, sourceUrl}){
             </IconButton>
           </Tooltip>
           <Tooltip title="Share" aria-label="Share">
-            <IconButton href="/" edge="start" color="inherit">
+            <IconButton onClick={()=>{ setDialogOpened(true); }} edge="start" color="inherit">
               <ShareIcon fontSize="small" />
             </IconButton>
           </Tooltip>
         </Toolbar>
       </AppBar>
+
+      <Dialog
+        fullScreen={fullScreen}
+        open={dialogOpened}
+        onClose={()=>{ setDialogOpened(false); }}
+        aria-labelledby="responsive-dialog-title"
+      >
+        <DialogTitle id="responsive-dialog-title">Share</DialogTitle>
+        <DialogContent>
+          <Button className={[classes.twitter, classes.snsButton]}>
+            <TwitterIcon />
+            Twitter
+          </Button>
+          <Button className={[classes.facebook, classes.snsButton]}>
+            <FacebookIcon />
+            Facebook
+          </Button>
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={()=>{ setDialogOpened(false); }} color="primary">
+            Disagree
+          </Button>
+          <Button onClick={()=>{ setDialogOpened(false); }} color="primary" autoFocus>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 };
