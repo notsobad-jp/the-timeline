99db2d5507ce5106ff4bdc730147509622e2cfed
notsobad <tomomichi.onishi@gmail.com>
2020-08-25 14:46:52 +0900

    fix no title error

--- app/lib/utils.js
+++ app/lib/utils.js
@@ -7,7 +7,8 @@ export async function getTitleFromSheet(gid) {
   const url = `https://docs.google.com/spreadsheets/d/e/${gid}/pubhtml`;
   const res = await fetch(url);
   const body = await res.text();
-  return body.match(/<title>(.*)<\/title>/)[1].replace(/ - Google (ドライブ|Drive)/, "");
+  const match = body.match(/<title>(.*)<\/title>/);
+  return match ? match[1].replace(/ - Google (ドライブ|Drive)/, "") : '';
 }
 
 export async function sheetsToJson(urls) {
