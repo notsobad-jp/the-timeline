70a024aa0cccd6e78b1be480c666bc4135e48a64
notsobad <tomomichi.onishi@gmail.com>
2020-08-30 16:53:37 +0900

    add share buttons

--- web/components/footer.js
+++ web/components/footer.js
@@ -1,10 +1,13 @@
 import React, { useContext, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Link from '../src/Link';
 import AppBar from '@material-ui/core/AppBar';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 import FormatAlignLeftIcon from '@material-ui/icons/FormatAlignLeft';
-import Link from '../src/Link';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import FacebookIcon from '@material-ui/icons/Facebook';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -31,6 +34,16 @@ const useStyles = makeStyles((theme) => ({
     marginRight: theme.spacing(1),
     verticalAlign: 'text-bottom',
   },
+  shareButtons: {
+
+  },
+  twitterButton: {
+    backgroundColor: 'rgb(90, 164, 235) !important',
+    marginRight: theme.spacing(1),
+  },
+  facebookButton: {
+    backgroundColor: 'rgb(59, 89, 152) !important',
+  },
 }));
 
 export default function Header(){
@@ -45,11 +58,15 @@ export default function Header(){
             THE TIMELINE
           </Link>
         </Typography>
-        <Box className={classes.footerLinks} mt={2}>
+        <Box mt={4}>
+          <Button href="https://twitter.com/intent/tweet?hashtags=the_timeline&text=%E5%B9%B4%E8%A1%A8%E4%BD%9C%E6%88%90%E3%82%B5%E3%83%BC%E3%83%93%E3%82%B9%E3%80%8CTHE%20TIMELINE%EF%BC%88%E3%82%B6%E3%83%BB%E3%82%BF%E3%82%A4%E3%83%A0%E3%83%A9%E3%82%A4%E3%83%B3%EF%BC%89%E3%80%8D&url=https%3A%2F%2Fthe-timeline.jp" component="a" target="_blank" variant="contained" color="primary" className={classes.twitterButton} size="small" startIcon={<TwitterIcon />}>ツイートする</Button>
+          <Button href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fthe-timeline.jp" component="a" target="_blank" variant="contained" color="primary" className={classes.facebookButton} size="small" startIcon={<FacebookIcon />}>シェアする</Button>
+        </Box>
+        <Box className={classes.footerLinks} mt={4}>
           <a href="/" target="_blank" color="inherit">利用規約</a>
           <a href="/" target="_blank" color="inherit">プライバシーポリシー</a>
           <a href="/" target="_blank" color="inherit">お問い合わせ</a>
-          <a href="/" target="_blank" color="inherit">運営会社</a>
+          <a href="https://notsobad.jp" target="_blank" color="inherit">運営会社</a>
         </Box>
         <Box className={classes.footerLinks} mt={2}>
           Copyright © THE TIMELINE 2016-{new Date().getFullYear()} All Rights Reserved.
