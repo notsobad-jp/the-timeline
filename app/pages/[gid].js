d0432e82ccacc3e162e2b81c52857d0b5d958d46
notsobad <tomomichi.onishi@gmail.com>
2020-09-10 15:43:50 +0900

    fix twitter card image

--- app/pages/[gid].js
+++ app/pages/[gid].js
@@ -40,6 +40,10 @@ export default function Index({title, data, sourceUrl, gid}) {
         <meta property="og:site_name" content="THE TIMELINE(ザ・タイムライン)" />
         <meta property="og:description" content={`${title} | 年表作成サービス「THE TIMELINE」`} />
         <meta property="og:locale" content="ja_JP" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:description" content={`${title} | 年表作成サービス「THE TIMELINE」`} />
+        <meta name="twitter:image:src" content={`${title} | 年表作成サービス「THE TIMELINE」`} />
+        <meta name="twitter:image:src" content={`${process.env.NEXT_PUBLIC_WEB_ROOT}/api/screenshot?gid=${gid}`} />
         <link rel="canonical" href={canonicalUrl} />
       </Head>
 
